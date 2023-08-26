d58b2c210e4c6bb02a3bf0fc6bba649717527ca2
Victorien Elvinger <victorien@elvinger.fr>
2023-08-26 22:37:11 +0200

    fix: script and background names

--- generate-png.sh
+++ generate-png.sh
@@ -2,24 +2,28 @@
 set -eu
 
 svg_to_png() {
-    gm convert -background none "$1" "${1%.*}.png"
-    pngquant -o out.png --strip --speed 1 "${1%.*}.png"
-    mv --force out.png "${1%.*}.png"
+    outfile="${1%.*}.png"
+    printf "Generate %s\n" "$outfile"
+    gm convert -background none "$1" "$outfile"
+    pngquant -o out.png --strip --speed 1 "$outfile"
+    mv --force out.png "$outfile"
 }
 
 svg_to_resized_png() {
-    gm convert -background none -resize "$2" "$1" "${1%.*}-$2.png"
-    pngquant -o out.png --strip --speed 1 "${1%.*}.png"
-    mv --force out.png "${1%.*}.png"
+    outfile="${1%.*}-$2.png"
+    printf "Generate %s\n" "$outfile"
+    gm convert -background none -resize "$2" "$1" "$outfile"
+    pngquant -o out.png --strip --speed 1 "$outfile"
+    mv --force out.png "$outfile"
 }
 
+rm ./*/*.png
+
 for f in ./*/*.svg; do
-    printf "Generate %s\n" "${f%.*}.png"
     svg_to_png "$f"
 done
 
 f=./icon/biome-icon.svg
 for size in 16x16 32x32 180x180 192x192 512x512; do
-    printf "Generate %s\n" "${f%.*}-$size.png"
     svg_to_resized_png "$f" "$size"
 done
